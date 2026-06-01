10dc5ea255231852285c52dd441e1927e0674b1e
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-06-01 21:39:51 +0200

    chore(openwrt): trim firmware packages

--- config.nix
+++ config.nix
@@ -3,7 +3,7 @@
   openwrtTarget = "mediatek";
   openwrtSubtarget = "filogic";
   openwrtProfile = "glinet_gl-mt6000";
-  openwrtPackages = "adguardhome irqbalance luci-ssl map kmod-wireguard wireguard-tools qrencode luci-proto-wireguard";
+  openwrtPackages = "adguardhome irqbalance map kmod-wireguard wireguard-tools";
   imageBuilderHash = "sha256-/n8Dt0jvac4L4RgvJ+PnLj3VJGMovQlB5CdKg3pWFCw=";
 
   routerHost = "10.10.0.1";
